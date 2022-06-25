ae15882a532f1a896f586f0290eb666c3655662a
François Steinel <francois.steinel@gmail.com>
2022-06-25 17:05:24 +0200

    feat, refactor: adjust project containers / infra

--- workspaces/client/src/pages/_app.tsx
+++ workspaces/client/src/pages/_app.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
 import { RecoilRoot } from 'recoil';
 import { SocketManagerProvider } from '@components/websocket/SocketManagerProvider';
-import { ToastContainer } from 'react-toastify';
+import { NotificationsProvider } from '@mantine/notifications';
 
 import '@styles/main.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -13,29 +13,40 @@ export default function _App(props: AppProps) {
 
   return (
     <RecoilRoot>
-      <SocketManagerProvider>
-        <Head>
-          <title>Memory Cards</title>
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
-          <meta name="description" content="Memory cards game"/>
-          <link rel="icon" href="/favicon.ico"/>
-        </Head>
+      <Head>
+        <title>Memory Cards</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+        <meta name="description" content="Memory cards game"/>
+        <link rel="icon" href="/cards.svg"/>
+      </Head>
 
-        <MantineProvider
-          withGlobalStyles
-          withNormalizeCSS
-          theme={{
-            colorScheme: 'dark',
-          }}
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        emotionOptions={{
+          key: 'mantine',
+          prepend: false,
+        }}
+        theme={{
+          colorScheme: 'dark',
+          radius: {
+            xs: 1,
+            sm: 1,
+            md: 1,
+            lg: 1,
+            xl: 1,
+          },
+        }}
+      >
+        <NotificationsProvider
+          position="top-right"
+          limit={15}
         >
-          <Component {...pageProps} />
-
-          <ToastContainer
-            newestOnTop
-            theme="dark"
-          />
-        </MantineProvider>
-      </SocketManagerProvider>
+          <SocketManagerProvider>
+            <Component {...pageProps} />
+          </SocketManagerProvider>
+        </NotificationsProvider>
+      </MantineProvider>
     </RecoilRoot>
   );
 }
